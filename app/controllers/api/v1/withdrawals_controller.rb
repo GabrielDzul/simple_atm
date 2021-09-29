54b4ab0f0fe3cc4442704633bd72553b76f695e0
Gabriel Dzul <gabrieldzul@gmail.com>
2021-09-29 12:36:22 -0500

    Added withdrawals flow and its tests

--- app/controllers/api/v1/withdrawals_controller.rb
+++ app/controllers/api/v1/withdrawals_controller.rb
@@ -3,7 +3,11 @@ module Api
     class WithdrawalsController < BaseController
 
       def create
+        schema = Schemas::V1::Withdrawals::Create.new
+        data = validate_request(schema: schema)
+        bank_notes = WithdrawalsServices::DispenseNotes.new.execute(data[:amount])
 
+        render json: bank_notes
       end
     end
   end
